845b898f73321750a8dcb53529c273fbc3f5355d
Ryan <RyanHoover159@gmail.com>
2024-01-24 00:28:32 -0500

    python

--- Module 1 Scripts/PythonExercisesRH/exercise_09.py
+++
@@ -1,9 +0,0 @@
-words_list = []
-
-for i in range(5):
-    words_list.append(input(f"Enter word #{i + 1}: "))
-
-result_string = ' '.join(words_list)
-
-print("List of words:", words_list)
-print("Resultant string:", result_string)
